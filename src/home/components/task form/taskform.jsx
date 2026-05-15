846c2666a59baf04adc3bdb0c70bde42e88d6e0e
Princewill Ubido <princewillubidoo@gmail.com>
2026-05-15 20:52:53 +0100

    UI update and array ordering

--- src/home/components/task form/taskform.jsx
+++ src/home/components/task form/taskform.jsx
@@ -1,100 +1,165 @@
-import { useEffect, useRef, useState } from 'react';
-import styles from './taskform.module.css';
+import { useEffect, useRef, useState } from "react";
+import styles from "./taskform.module.css";
 
-const TaskForm = ({ todos, setTodos, showForm, setShowForm, error, setError}) => {
+const TaskForm = ({
+  todos,
+  setTodos,
+  showForm,
+  setShowForm,
+  error,
+  setError,
+}) => {
+  // Closes form when escape key is clicked
+  useEffect(() => {
+    const handleEscape = (e) => {
+      if (e.key === "Escape") setShowForm(false);
+    };
+    window.addEventListener("keydown", handleEscape);
+    return () => window.removeEventListener("keydown", handleEscape);
+  }, [setShowForm]);
 
-    // Closes form when escape key is clicked
-    useEffect(() => {
-        const handleEscape = (e) => {
-            if (e.key === "Escape") setShowForm(false)
+  // Handles focus to input on mount
+  const taskInputRef = useRef(null);
+  useEffect(() => {
+    if (showForm) {
+      taskInputRef.current?.focus();
+    }
+  }, [showForm]);
+
+  // Todo object and input collection handling
+  const [todo, setTodo] = useState({
+    id: crypto.randomUUID(),
+    task: "",
+    priority: "",
+    completed: false,
+  });
+
+  const handleTodo = (e) => {
+    setTodo({
+      ...todo,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Form validation and error handling
+  const validate = () => {
+    const newError = {};
+
+    if (todo.task === "") {
+      newError.task = "Task cannot be empty";
+    }
+
+    return newError;
+  };
+
+  // Function that adds todo
+  const addTodo = (newTodo) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = [...prevTodos];
+
+      if (newTodo.priority === "priority1") {
+        // insert at very top
+        updatedTodos.unshift(newTodo);
+      } else if (newTodo.priority === "priority2") {
+        // after all priority1 tasks
+        const index = updatedTodos.findIndex(
+          (todo) => todo.priority !== "priority1",
+        );
+
+        if (index === -1) {
+          updatedTodos.push(newTodo);
+        } else {
+          updatedTodos.splice(index, 0, newTodo);
         }
-        window.addEventListener("keydown", handleEscape)
-        return () => window.removeEventListener("keydown", handleEscape)
-    }, [setShowForm])
+      } else if (newTodo.priority === "priority3") {
+        // after priority1 and priority2
+        const index = updatedTodos.findIndex(
+          (todo) =>
+            todo.priority !== "priority1" && todo.priority !== "priority2",
+        );
 
-    // Handles focus to input on mount
-    const taskInputRef = useRef(null);
-    useEffect(() => {
-        if(showForm) {
-            taskInputRef.current?.focus()
+        if (index === -1) {
+          updatedTodos.push(newTodo);
+        } else {
+          updatedTodos.splice(index, 0, newTodo);
         }
-    }, [showForm])
+      } else {
+        // no priority -> bottom
+        updatedTodos.push(newTodo);
+      }
 
+      return updatedTodos;
+    });
+  };
 
-    // Todo object and input collection handling
-    const [todo, setTodo] = useState({
-        id: crypto.randomUUID(),
+  // Function that handles submission of form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationError = validate();
+
+    if (Object.keys(validationError).length > 0) {
+      setError(validationError);
+    } else {
+      addTodo(todo);
+      setShowForm(false);
+      setTodo({
+        id: Date.now(),
         task: "",
         priority: "",
-        completed: false
-    });
-
-    const handleTodo = (e) => {
-        setTodo({
-            ...todo,
-            [e.target.name]: e.target.value
-        })
+        completed: false,
+      });
+      setError({});
     }
+  };
 
-    // Form validation and error handling
-    const validate = () => {
-        const newError = {};
+  if (!showForm) return null;
 
-        if (todo.task === '') {
-            newError.task = 'Task cannot be empty'
-        }
-        
-        return newError;
-    }
-
-    // Function that adds todo
-    const addTodo = (newTodo) => {
-        setTodos([...todos, newTodo])
-    }
-
-    // Function that handles submission of form
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        const validationError = validate();
-
-        if (Object.keys(validationError).length > 0 ) {
-            setError(validationError);
-        } else {
-            addTodo(todo)
-            setShowForm(false);
-            setTodo({
-                id: Date.now(),
-                task: "",
-                priority: "",
-                completed: false
-            })
-            setError({});
-        }
-    }
-
-    if (!showForm) return null;
-
-    return(
-        <div onClick={() => setShowForm(false)} className={styles.backdrop}>
-        <div className={styles.taskFormContainer} onClick={(e) => e.stopPropagation()}>
-            <form action="" className={styles.addTaskForm}>
-                <div>
-                    <input type="text" name="task" className={styles.taskInput} placeholder="Enter new task" onChange={handleTodo} value={todo.task} aria-label='Enter task' ref={taskInputRef}/>
-                </div>
-                <div className={styles.formActions}>
-                    <select name="priority" className={styles.priority} onChange={handleTodo} value={todo.priority} aria-label='Select task priority'>
-                        <option value="">Priority</option>
-                        <option value="priority1">Priority 1</option>
-                        <option value="priority2">Priority 2</option>
-                        <option value="priority3">Priority 3</option>
-                    </select>
-                    <button type="submit" className={styles.addTaskBtn} onClick={handleSubmit} aria-label='Add task'>Add</button>
-                </div>
-            </form>
-            { error.task && <p className={styles.errMsg}>{error.task}</p>}
-        </div>
-        </div>
-    )
+  return (
+    <div onClick={() => setShowForm(false)} className={styles.backdrop}>
+      <div
+        className={styles.taskFormContainer}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <form action="" className={styles.addTaskForm}>
+          <div className={styles.formInput}>
+            <input
+              type="text"
+              name="task"
+              className={styles.taskInput}
+              placeholder="Enter new task"
+              onChange={handleTodo}
+              value={todo.task}
+              aria-label="Enter task"
+              ref={taskInputRef}
+            />
+          </div>
+          <div className={styles.formActions}>
+            <select
+              name="priority"
+              className={styles.priority}
+              onChange={handleTodo}
+              value={todo.priority}
+              aria-label="Select task priority"
+            >
+              <option value="">Priority</option>
+              <option value="priority1">Urgent</option>
+              <option value="priority2">Important</option>
+              <option value="priority3">Normal</option>
+            </select>
+            <button
+              type="submit"
+              className={styles.addTaskBtn}
+              onClick={handleSubmit}
+              aria-label="Add task"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+        {error.task && <p className={styles.errMsg}>{error.task}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default TaskForm;
--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,80 +1,110 @@
-import styles from './home.module.css';
-import TaskForm from './components/task form/taskform';
-import { useEffect, useRef, useState } from 'react';
+import styles from "./home.module.css";
+import TaskForm from "./components/task form/taskform";
+import { useEffect, useRef, useState } from "react";
 
 const Home = () => {
+  // Function that handles show & hide form
+  const [showForm, setShowForm] = useState(false);
+  const newTask = () => {
+    setShowForm(!showForm);
+    setError({});
+  };
 
-    // Function that handles show & hide form
-    const [showForm, setShowForm] = useState(false);
-    const newTask = () => {
-        setShowForm(!showForm);
-        setError({});
+  // Errors
+  const [error, setError] = useState({});
+
+  // Function that handles the color of the task based on priority
+  const taskPriority = (priority) => {
+    if (priority === "priority1") return "#dd4108";
+    if (priority === "priority2") return "#ffa500";
+    if (priority === "priority3") return "#325aed";
+    return "#ffff";
+  };
+
+  // Array that stores todos.
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(todos);
+  }, []);
+
+  const isFirstRender = useRef(true);
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
     }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
-    // Errors
-    const [error, setError] = useState({});
+  // Function that handles task complete
+  const taskComplete = (id) => {
+    const updatedTodos = todos.map((todo) =>
+      todo.id === id ? { ...todo, completed: true } : todo
+    );
+    setTodos(updatedTodos);
 
+    setTimeout(() => {
+      const completedTodos = todos.filter((todo) => todo.id !== id);
+      setTodos(completedTodos);
+    }, 500);
+  };
 
-    // Function that handles the color of the task based on priority
-    const taskPriority = (priority) => {
-        if (priority === 'priority1') return '#dd4108';
-        if (priority === 'priority2') return '#ffa500';
-        if (priority === 'priority3') return '#325aed';
-        return '#ffff';
-    } 
-
-    // Array that stores todos.
-    const [todos, setTodos] = useState([]);
-    useEffect(() => {
-        const todos = JSON.parse(localStorage.getItem('todos')) || []
-        setTodos(todos)
-    }, []);
-
-    const isFirstRender = useRef(true);
-    useEffect(() => {
-
-        if (isFirstRender.current) {
-            isFirstRender.current = false;
-            return;
-        }
-        localStorage.setItem('todos', JSON.stringify(todos))
-    }, [todos]);
-
-
-    // Function that handles task complete
-    const taskComplete = (id) => {
-        const updatedTodos = todos.map(todo => todo.id === id ? {...todo, completed: true } : todo)
-        setTodos(updatedTodos)
-
-        setTimeout(() => {
-            const completedTodos = todos.filter( todo => todo.id !== id);
-            setTodos(completedTodos);
-        }, 500)
-    };
-
-    return(
-        <div className={styles.homeContainer}>
-            <div className={styles.header}>
-                <h1>Todo App</h1>
+  return (
+    <div className={styles.homeContainer}>
+      <div className={styles.header}>
+        <h1>Todo App</h1>
+      </div>
+      <div className={styles.newTaskBtn} onClick={newTask}>
+        <i className="bx bx-plus"></i>
+      </div>
+      <div className={styles.taskList}>
+        {todos.length === 0 ? (
+          <p
+            className={styles.noTask}
+            style={{ display: showForm === true ? "none" : "flex" }}
+          >
+            Click + to add task
+          </p>
+        ) : (
+          todos.map((todo) => (
+            <div
+              className={
+                todo.completed === true
+                  ? `${styles.task} ${styles.completed}`
+                  : styles.task
+              }
+              key={todo.id}
+              style={{ color: taskPriority(todo.priority) }}
+              id={`task-${todo.id}`}
+            >
+              <input
+                type="radio"
+                className={styles.taskComplete}
+                style={{ color: taskPriority(todo.priority) }}
+                onChange={() => taskComplete(todo.id)}
+              />
+              <p>{todo.task}</p>
             </div>
-            <div className={styles.newTaskBtn} onClick={newTask}>
-                <i className='bx bx-plus'></i>
-            </div>
-            <div className={styles.taskList}>
-                {todos.length === 0 ? <p className={styles.noTask} style={{display: showForm === true ? 'none' : 'flex'}}>Click + to add task</p> : todos.map((todo) => (
-                    <div className={ todo.completed === true ? `${styles.task} ${styles.completed}`: styles.task } key={todo.id} style={{color: taskPriority(todo.priority)}} id={`task-${todo.id}`}>
-                        <input type="radio" className={styles.taskComplete} style={{ color: taskPriority(todo.priority)}} onChange={() => taskComplete(todo.id)}/>
-                        <p>{todo.task}</p>
-                    </div>
-                ))}
-            </div>
-            <TaskForm showForm={showForm} setShowForm={setShowForm} todos={todos} setTodos={setTodos} error={error} setError={setError}/>
-            <div className={styles.footerSection}>
-                <p>Created by</p>
-                <a href="https://x.com/builtbyMaro_" target='_blank'>Maro</a>
-            </div>
-        </div>
-    )
+          ))
+        )}
+      </div>
+      <TaskForm
+        showForm={showForm}
+        setShowForm={setShowForm}
+        todos={todos}
+        setTodos={setTodos}
+        error={error}
+        setError={setError}
+      />
+      <div className={styles.footerSection}>
+        <p>Created by</p>
+        <a href="https://x.com/builtbyMaro_" target="_blank">
+          Maro
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
